296f2027f213478970fdd2af917ec53c54b60812
王慧 <@asus>
2016-10-24 15:42:59 +0800

    passwordmain

--- src/compassword/main.java
+++ src/compassword/main.java
@@ -6,17 +6,16 @@ public class main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-String pa;
-		
+        String pa;
 		do{
-		Scanner input=new Scanner(System.in);
-		System.out.print("please enter a password:");
-		pa=input.next();
-		PasswordEncorder cor=new PasswordEncorder();
-		cor.encode(pa);
-		System.out.println("");
-		}while(pa.charAt(0)!='e'&&pa.charAt(1)!='x'&&pa.charAt(2)!='i'&&pa.charAt(3)!='t'&&pa.charAt(4)==' ');
-
+			Scanner input=new Scanner(System.in);
+			System.out.print("please enter a password:");
+			pa=input.next();
+			PasswordEncorder cor=new PasswordEncorder();
+			cor.encode(pa);
+			System.out.println("");
+		}while(pa!=("exit"));
+		
 	}
 
 
